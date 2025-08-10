7b23b60753b40cb05603774b2b994f7a8ad260e6
sagar <sagar@einsicht-technologies.com>
2025-08-11 00:01:06 +0530

    error

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /**
  * @format
  */
+import 'react-native-gesture-handler'; // ✅ must be first import
+import 'react-native-reanimated'; // ✅ reanimated import here
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function Root() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <App />
+    </GestureHandlerRootView>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Root);
